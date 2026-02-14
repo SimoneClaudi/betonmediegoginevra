f606dc0c26e9957ecb24cc5ba5f477d418b86c11
SimoneClaudi <66772617+SimoneClaudi@users.noreply.github.com>
2026-02-14 18:28:19 +0100

    init
    init

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1swqOth9iLtdRYZaz7SA4Oi8_A9Zb7ps",
+  authDomain: "betonmediegoginevra.firebaseapp.com",
+  databaseURL: "https://betonmediegoginevra-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "betonmediegoginevra",
+  storageBucket: "betonmediegoginevra.appspot.com",
+  messagingSenderId: "190234771827",
+  appId: "1:190234771827:web:7675df878b3cda30d04506",
+  measurementId: "G-S5MLJF6JET"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
